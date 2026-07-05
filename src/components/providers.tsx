6600072a252fc5f400bba432421b714ff2926dff
Adones Balica <adonesbalica@gmail.com>
2026-07-05 00:47:58 -0300

    feat: integrate @tanstack/react-query and sonner for enhanced state management and notifications

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,9 +1,26 @@
+import { QueryClient } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactNode } from 'react'
+import { toast } from "sonner";
+import { Toaster } from "./ui/sonner";
 
 type ProvidersProps = {
   children: ReactNode
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (e) => {
+        if (e.message === "NEXT_REDIRECT") return;
+        toast.error(e.message);
+      },
+      onSuccess: () => {
+        toast.success("Operation was successful.")
+      }
+    }
+  }
+})
 
 const Providers = ({ children }: ProvidersProps) => {
   return (
@@ -13,7 +30,8 @@ const Providers = ({ children }: ProvidersProps) => {
         enableSystem
         disableTransitionOnChange
       >
-      {children}
+        <Toaster/>
+        {children}
     </NextThemesProvider>
   )
 }
